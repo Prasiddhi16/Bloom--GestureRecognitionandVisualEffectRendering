4744ae4555b4b41d4b62398949fa3328e6d0165e
Prasiddhi16 <dumreprasiddhi@gmail.com>
2026-07-01 20:34:29 +0545

    Added new

---
+++ practicee.py
@@ -0,0 +1,10 @@
+course_name = "Python Programming"
+print (len(course_name))
+print(course_name[0:3])
+first="Prasiddhi"
+last="Dumre"
+full=first+" "+last
+print(full)
+x= input("x:")
+y=int(x)+1
+print(y)

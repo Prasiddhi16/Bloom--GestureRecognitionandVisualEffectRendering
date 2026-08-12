2c26dab01bcc19876d0b4108f8cc55f05740a895
Prasiddhi16 <dumreprasiddhi@gmail.com>
2026-08-12 20:10:21 +0545

    Added

---
+++ queue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct 
+{
+    int data;
+    int front;
+    int rear;
+   
+}Queue;
+void initqueue(Queue *q)
+{
+    q->front=-1;
+    q->rear=-1;
+}
+int is
+void enqueue(Queue *q,int data)
+{
+
+}

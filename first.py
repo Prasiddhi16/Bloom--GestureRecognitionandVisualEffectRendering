198b9f4534d8ee0423847955ae0818bd945928c0
Prasiddhi16 <dumreprasiddhi@gmail.com>
2026-08-18 06:50:09 +0545

    Added

---
+++ first.py
@@ -0,0 +1,30 @@
+# print ("hello world\n its a good day!")
+# #good morning all good?
+# '''its all been like a dream to go to a big day isnt it life is fukllbvhdgdskdj
+# shasgfhgfdsbffhujdbvdbcdcbbdfhgdsfsfgjk'''
+# print("hetty",6,7,8,sep="@",end="hehehe\n")
+# print("life is hectic isnt")
+
+#printing twinkle twinkle little star using python
+print("Twinkle Twinkle Little star\n")
+print("Twinkle twinkle little star\nHow i wonder what you are?\nUp above the world so high\nLike a diamond in the sky!! ")
+print("\nend!")
+a=1234
+print(a)
+b=785
+c="siddhi"
+print(c)
+print(a+b)
+print("the type of a is",type(a))
+print("the type of a is",type(c))
+e=23
+f=45
+print("addition=",a+b)
+print("subtraction=",a-b)
+print("division=",a/b)
+print("modulus=",a%b)
+print("float=",a//b)
+names="hari,ram"
+print(names[0:5])
+fruit= "mango"
+print(fruit[-4:-2])
